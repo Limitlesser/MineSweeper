c5b1227e48a3867f5ca15e2943f3a79954eeef6d
Limitlesser <kang34814@gmail.com>
2017-06-01 15:39:43 +0800

    fix

--- app/src/main/java/wind/minesweeper/MineFieldView.kt
+++ app/src/main/java/wind/minesweeper/MineFieldView.kt
@@ -25,8 +25,7 @@ class MineFieldView(context: Context, val mineField: MineField) : View(context) 
         override fun onDown(e: MotionEvent?) = true
 
         override fun onSingleTapUp(e: MotionEvent): Boolean {
-            val point = toMinePoint(e.x, e.y)
-            mineField.show(point.x, point.y)
+            mineField.show((e.x / cell.x).toInt(), (e.y / cell.y).toInt())
             invalidate()
             return true
         }
@@ -40,29 +39,30 @@ class MineFieldView(context: Context, val mineField: MineField) : View(context) 
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
-        for (i in 0..(mineField.width - 1))
-            for (j in 0..(mineField.height - 1)) {
+        for (i in 0..mineField.width - 1)
+            for (j in 0..mineField.height - 1) {
                 val block = mineField.blocks[i][j]
-                val point = toViewPoint(i, j)
-                if (block.open) {
-                    if (block.bomb) {
+                val x = (i * cell.x).toFloat()
+                val y = (j * cell.y).toFloat()
+                if (block.isOpen) {
+                    if (block.isBomb) {
                         paint.color = Color.WHITE
-                        drawBlock(canvas, point)
+                        drawBlock(canvas, x, y)
                         paint.color = Color.RED
-                        canvas.drawCircle(point.x.toFloat() + cell.x / 2, point.y.toFloat() + cell.y / 2
+                        canvas.drawCircle(x + cell.x / 2, y + cell.y / 2
                                 , Math.min(cell.x, cell.y) / 2.toFloat() - 5, paint)
                     } else {
                         paint.color = Color.WHITE
-                        drawBlock(canvas, point)
+                        drawBlock(canvas, x, y)
                         paint.color = Color.DKGRAY
                         paint.textSize = 20f
-                        if (block.bombNum > 0)
-                            canvas.drawText(block.bombNum.toString(), point.x.toFloat() + cell.x / 2 - 3,
-                                    point.y.toFloat() + cell.y / 2 + 5, paint)
+                        if (block.aroundBombNum > 0)
+                            canvas.drawText(block.aroundBombNum.toString(), x + cell.x / 2 - 3,
+                                    y + cell.y / 2 + 5, paint)
                     }
                 } else {
                     paint.color = Color.DKGRAY
-                    drawBlock(canvas, point)
+                    drawBlock(canvas, x, y)
                 }
             }
     }
@@ -71,12 +71,8 @@ class MineFieldView(context: Context, val mineField: MineField) : View(context) 
         return gestureDetector.onTouchEvent(event)
     }
 
-    private fun drawBlock(canvas: Canvas, point: Point) =
-            canvas.drawRect(point.x.toFloat() + 3, point.y.toFloat() + 3,
-                    (point.x + cell.x).toFloat() - 3, (point.y + cell.y).toFloat() - 3, paint)
+    private fun drawBlock(canvas: Canvas, x: Float, y: Float) =
+            canvas.drawRect(x + 3, y + 3, (x + cell.x) - 3, (y + cell.y) - 3, paint)
 
 
-    private fun toViewPoint(x: Int, y: Int) = Point(x * cell.x, y * cell.y)
-
-    private fun toMinePoint(x: Float, y: Float) = Point((x / cell.x).toInt(), (y / cell.y).toInt())
 }
--- app/src/main/java/wind/minesweeper/MineSweeper.kt
+++ app/src/main/java/wind/minesweeper/MineSweeper.kt
@@ -6,7 +6,7 @@ import java.util.*
  * Created by wind on 2017/6/1.
  */
 
-data class Block(val bomb: Boolean, var open: Boolean = false, var bombNum: Int = 0)
+data class Block(val isBomb: Boolean, var isOpen: Boolean = false, var aroundBombNum: Int = 0)
 
 class MineField(val width: Int, val height: Int, bombNum: Int) {
 
@@ -27,13 +27,13 @@ class MineField(val width: Int, val height: Int, bombNum: Int) {
 
     fun show(x: Int, y: Int) {
         val block = blocks[x][y]
-        if (!block.open) {
-            block.open = true
-            if (block.bomb) {
-                blocks.forEach { it.forEach { it.open = true } }
+        if (!block.isOpen) {
+            block.isOpen = true
+            if (block.isBomb) {
+                blocks.forEach { it.forEach { it.isOpen = true } }
             } else {
-                block.bombNum = aroundBombs(x, y).filter { it.bomb }.size
-                if (block.bombNum == 0) {
+                block.aroundBombNum = aroundBombs(x, y).filter { it.isBomb }.size
+                if (block.aroundBombNum == 0) {
                     aroundBombs(x, y) { i, j -> show(i, j) }
                 }
             }
@@ -43,9 +43,9 @@ class MineField(val width: Int, val height: Int, bombNum: Int) {
 
     private fun aroundBombs(x: Int, y: Int, block: ((Int, Int) -> Unit)? = null): List<Block> {
         val bombs = mutableListOf<Block>()
-        for (i in (x - 1)..(x + 1))
-            for (j in (y - 1)..(y + 1)) {
-                if (i in 0..(width - 1) && j in 0..(height - 1)) {
+        for (i in x - 1..x + 1)
+            for (j in y - 1..y + 1) {
+                if (i in 0..width - 1 && j in 0..height - 1) {
                     bombs.add(blocks[i][j])
                     block?.invoke(i, j)
                 }
